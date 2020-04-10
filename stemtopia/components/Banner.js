901a4a060c0fa95522d42411284cd827e686dbdd
Wenbo Sun <wenbosun@Wenbos-MacBook-Pro.local>
2020-04-10 16:12:33 -0400

    test

---
+++ stemtopia/components/Banner.js
@@ -0,0 +1,21 @@
+const Banner = (props) => (
+  <section id="banner" className="major">
+    <div className="inner">
+      <header className="major">
+        <h1>Welcome to Stemtopia</h1>
+      </header>
+      <div className="content">
+        <p>A AI based computational thinking class for kid age from 10-14.</p>
+        <ul className="actions">
+          <li>
+            <a href="#one" className="button next scrolly">
+              Get Started
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+);
+
+export default Banner;
